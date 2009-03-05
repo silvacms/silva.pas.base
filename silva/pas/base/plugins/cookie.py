fca77282bc20bea5e6beb3802f04ae0dddf6f1f9
Sylvain Viollon <thefunny@gmail.com>
2009-03-05 09:13:28 +0000

    Fix in case this remove everything.

--- silva/pas/base/plugins/cookie.py
+++ silva/pas/base/plugins/cookie.py
@@ -70,9 +70,10 @@ class SilvaCookieAuthHelper(CookieAuthHelper):
                     for bad in ['login_status', '-C']:
                         if bad in query:
                             del query[bad]
-                    keys, values = zip(*query.items())
-                    query = dict(zip(keys, map(lambda v: v.encode('ascii', 'xmlcharrefreplace'), values)))
-                    came_from = mangle.urlencode(came_from, **query)
+                    if query:
+                        keys, values = zip(*query.items())
+                        query = dict(zip(keys, map(lambda v: v.encode('ascii', 'xmlcharrefreplace'), values)))
+                        came_from = mangle.urlencode(came_from, **query)
             else:
                 req_url = req.get('URL', '')
 
