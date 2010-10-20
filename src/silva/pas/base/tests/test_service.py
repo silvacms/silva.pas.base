e6ae1460337d94e788f983edc562e8a1c5e20a44
Sylvain Viollon <thefunny@gmail.com>
2010-10-20 14:02:06 +0000

    Add some more tests.

--- src/silva/pas/base/tests/test_service.py
+++ src/silva/pas/base/tests/test_service.py
@@ -4,12 +4,13 @@
 
 import unittest
 
-from Acquisition import aq_base
 from Products.Silva import roleinfo
+from Products.PluggableAuthService.interfaces import plugins
 from zope.component import queryUtility
 from zope.interface.verify import verifyObject
 
 from silva.core.services import interfaces
+from silva.core.interfaces.auth import IGroup, IMember
 from silva.pas.base.testing import FunctionalLayer
 
 
@@ -21,6 +22,10 @@ class ServiceTestCase(unittest.TestCase):
     def setUp(self):
         self.root = self.layer.get_application()
         self.layer.login('manager')
+
+        # Add a test user.
+        pas_acl = self.root.acl_users
+        pas_acl._doAddUser('luser', 'luser', [], [])
 
     def test_service(self):
         """Install should change the members services and set up an
@@ -35,12 +40,21 @@ class ServiceTestCase(unittest.TestCase):
         self.assertEqual(
             service.meta_type,
             "Silva Pluggable Auth Service Member Service")
+        self.assertEqual(service, self.root.service_members)
         self.assertTrue(verifyObject(interfaces.IMemberService, service))
+
+        service = queryUtility(interfaces.IGroupService)
+        self.assertEqual(
+            service.meta_type,
+            "Silva Pluggable Auth Service Member Service")
+        self.assertEqual(service, self.root.service_members)
         #self.assertTrue(verifyObject(interfaces.IGroupService, service))
 
     def test_acl_user(self):
+        """Verify that a the acl_users have been added and configured
+        by the installation script.
+        """
         # And a acl_users set
-        self.assertTrue(hasattr(aq_base(self.root), 'acl_users'))
         pas_acl = self.root.acl_users
         self.assertEqual(
             pas_acl.objectIds(),
@@ -52,8 +66,104 @@ class ServiceTestCase(unittest.TestCase):
             set(p['id'] for p in pas_acl.roles.enumerateRoles()),
             set(('Owner',) + roleinfo.ASSIGNABLE_ROLES))
 
+    def test_invalid_member(self):
+        """Looking for a users that doesn't exists won't return anything.
+        """
+        service = queryUtility(interfaces.IMemberService)
+        self.assertEqual(service.is_user('winner'), False)
+        self.assertEqual(service.get_member('winner'), None)
+        self.assertEqual(service.find_members('winner'), [])
+
+    def test_valid_member(self):
+        """Looking for a member will actually return one.
+        """
+        service = queryUtility(interfaces.IMemberService)
+        self.assertEqual(service.is_user('luser'), True)
+
+        member = service.get_member('luser')
+        self.assertNotEqual(member, None)
+        self.assertEqual(member.userid(), 'luser')
+        self.assertEqual(member.fullname(), 'luser')
+        self.assertTrue(verifyObject(IMember, member))
+
+        self.assertEqual(service.find_members('luser'), [member])
+
+    def test_valid_acquired_member(self):
+        """Look for a member that is not defined in the site
+        acl_users, but at the root of Zope. It should by found with
+        the help of the cascading plugin.
+        """
+        service = queryUtility(interfaces.IMemberService)
+        self.assertEqual(service.is_user('manager'), True)
+
+        member = service.get_member('manager')
+        self.assertNotEqual(member, None)
+        self.assertEqual(member.userid(), 'manager')
+        self.assertEqual(member.fullname(), 'manager')
+        self.assertTrue(verifyObject(IMember, member))
+
+        self.assertEqual(service.find_members('manager'), [member])
+
+    def test_group_disable_by_default(self):
+        """By default there is no group source, so group are not
+        activated.
+        """
+        service = queryUtility(interfaces.IGroupService)
+        self.assertEqual(service.use_groups(), False)
+
+
+class GroupEnabledServiceTestCase(unittest.TestCase):
+    """Test case for PAS install/deinstall.
+    """
+    layer = FunctionalLayer
+
+    def setUp(self):
+        self.root = self.layer.get_application()
+        self.layer.login('manager')
+
+        # Add and configure a group backend in the acl_users
+        pas_acl = self.root.acl_users
+        factory = pas_acl.manage_addProduct['PluggableAuthService']
+        factory.addZODBGroupManager('groups')
+        pas_acl.groups.addGroup('losers', 'Losing users')
+        pas_acl.plugins.activatePlugin(
+            plugins.IGroupEnumerationPlugin, 'groups')
+
+    def test_invalid_group(self):
+        """Looking for a group that doesn't exists won't return
+        anything.
+        """
+        service = queryUtility(interfaces.IGroupService)
+
+        # So now groups should be on
+        self.assertEqual(service.use_groups(), True)
+
+        # If a group doesn't exists you won't find it.
+        self.assertEqual(service.is_group('winners'), False)
+        self.assertEqual(service.get_group('winners'), None)
+        self.assertEqual(service.find_groups('winners'), [])
+
+    def test_valid_group(self):
+        """Looking for a valid group will return something.
+        """
+        service = queryUtility(interfaces.IGroupService)
+
+        # So now groups should be on
+        self.assertEqual(service.use_groups(), True)
+
+        # If a group doesn't exists you won't find it.
+        self.assertEqual(service.is_group('losers'), True)
+
+        group = service.get_group('losers')
+        self.assertNotEqual(group, None)
+        self.assertEqual(group.groupid(), 'losers')
+        self.assertEqual(group.groupname(), 'Losing users')
+        self.assertTrue(verifyObject(IGroup, group))
+        self.assertEqual(len(service.find_groups('losers')), 1)
+
 
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(ServiceTestCase))
+    suite.addTest(unittest.makeSuite(GroupEnabledServiceTestCase))
     return suite
