48c356a85271f276ae94467c190e755bc7d4797a
Andy Altepeter <aaltepet@altepeter.net>
2011-05-04 11:03:11 -0500

    improve PASMember support

--- src/silva/pas/base/pasmember.py
+++ src/silva/pas/base/pasmember.py
@@ -11,9 +11,21 @@ from Products.Silva.SimpleMembership import SimpleMember
 from zeam.form import silva as silvaforms
 from Products.PluggableAuthService.interfaces.authservice import \
     IPluggableAuthService
+from Products.PluggableAuthService.interfaces.plugins import \
+    IPropertiesPlugin
+
+marker = object()
 
 class PASMember(SimpleMember):
-    """PAS member get's it's attributes from the PAS user objects"""
+    """PAS member get's it's attributes from the PAS user objects
+    
+       If property doesn't exist in PAS, pull it from
+       SimpleMember.  I can't think of a better, automated way to
+       allow empty pas properties to be empty, falling back to Member
+       properties.  (PAS properties are fine for ldap or radius, but
+       local zope users in a pas don't have these properties, and they
+       still need to be set-able.
+    """
 
     silvaconf.factory('manage_addPASMember')
     silvaconf.icon('member.png')
@@ -22,26 +34,58 @@ class PASMember(SimpleMember):
     
     def __init__(self, id):
         super(PASMember, self).__init__(id)
-        self._email = "ldap"
         
-    def email(self):
-        """email from pas"""
+    def __get_property__(self, prop, empty=marker):
+        """Get a property from the PAS user.
+
+           PAS users are PropertyManagers, so getProperty _should_ work
+           to retrieve properties from the IPropertiesPlugins, but it does
+           not.  Instead, you need to retrieve the appropriate propertysheet
+           and get the property from the sheet directly.
+        """
         pas = getattr(self, 'acl_users')
         if not IPluggableAuthService.providedBy(pas):
             raise RuntimeError, "Expect to be used with a PAS acl user"
         user = pas.getUser(self.userid())
-        user.getProperty('first_name')
-        # also listPropertysheets() and propertyIds()
-        return 'blah'
+        #listPlugins returns a 2-tuple of (plugin id, plugin obj)
+        plugins = pas.plugins.listPlugins(IPropertiesPlugin)
+        for (pid,plug) in plugins:
+            try:
+                return user.getPropertysheet(pid).getProperty(prop)
+            except KeyError:
+                pass
+        return marker
+        
+    def email(self):
+        """email from pas."""
+        prop = self.__get_property__('email')
+        if prop is marker: #property not in PAS
+            return super(PASMember, self).email()
+        return prop
     
     def fullname(self):
         """fullname from pas"""
+        prop = self.__get_property__('fullname')
+        if prop is marker: #property not in PAS
+            return super(PASMember, self).fullname()
+        return prop
         
     def set_fullname(self, fullname):
-        raise NotImplemented
+        """only allow setting fullname if there is no fullname pas property"""
+        if self.__get_property__('fullname') is not marker:
+            raise NotImplemented
+        return super(PASMember, self).set_fullname(fullname)
     
-    def set_email(self):
-        raise NotImplemented
+    def set_email(self, email):
+        """only allow setting fullname if there is no fullname pas property"""
+        if self.__get_property__('email') is not marker:
+            raise NotImplemented
+        return super(PASMember, self).set_email(email)
+    
+    def can_edit_property(self, prop):
+        """returns True if the property is editable.  This is only possible
+           if the property for the user does not exist in PAS"""
+        return self.__get_property__(prop) is marker
 
 
 def manage_addPASMember(self, id, REQUEST=None):
@@ -64,7 +108,8 @@ class PASMemberFactory(grok.Adapter):
 class PASUserInfoFieldsFactory(SimpleUserInfoFieldsFactory):
     """factory for returning the silvaforms.Fields schema for PASMembers.
     PAS schema is the SimpleUserInfo schema, but with most fields disabled
-    (since they're sourced from PAS and readonly.
+    (since they're sourced from PAS and readonly.  If the property is editable
+    (i.e. it is NOT in PAS), leave it editable.
     XXX it seems like PAS does allow for non-reaonly attributes, so we may
         want to look in to improving this factory, setting only those
         attributes which are actually readonly in pas as readonly in the form
@@ -73,8 +118,10 @@ class PASUserInfoFieldsFactory(SimpleUserInfoFieldsFactory):
     
     def get_fields(self):
         fields = super(PASUserInfoFieldsFactory, self).get_fields()
-        fields['fullname'].mode = silvaforms.DISPLAY
-        fields['email'].mode = silvaforms.DISPLAY
-        #language and avatar ARE STILL editable
+        #these fields are editable if they do not exist in PAS
+        if not self.context.can_edit_property('fullname'):
+            fields['fullname'].mode = silvaforms.DISPLAY
+        if not self.context.can_edit_property('email'):
+            fields['email'].mode = silvaforms.DISPLAY
         return fields
 
